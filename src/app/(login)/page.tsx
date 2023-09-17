c56072c45dc132b59296ea78edf42cf3211e75bc
Ramonprog <programador.ramon@gmail.com>
2023-09-17 12:30:32 -0300

    git commit -m "first commit"

---
+++ src/app/(login)/page.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Logo from '../../../public/logo.png'
+import { InputCustomComponent } from '@/components/ui/custom/InputCustomComponent'
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className='w-full h-full flex flex-col items-center mt-10'>
+
+      <Image src={Logo} height={300} width={300} alt='Logo da fratelli pizzaria' />
+
+      <div className='w-96 m-auto'>
+        <div className='mb-5'>
+          <InputCustomComponent type='text' htmlfor='email' label='E-mail' placeholder='Digite seu e-mail' />
+        </div>
+
+        <div>
+          <InputCustomComponent type='password' htmlfor='password' label='Senha' placeholder='Digite sua senha' />
+        </div>
+      </div>
+
+      <Button className='mt-8'>Acessar</Button>
+    </div>
+  )
+}
---
+++ src/components/ui/custom/InputCustomComponent.tsx
@@ -0,0 +1,20 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { type } from "os"
+
+type InputCustomComponentProps = {
+    type: string
+    label: string
+    htmlfor: string
+    width?: string
+    placeholder?: string
+}
+
+export function InputCustomComponent({ type, label, htmlfor, width, placeholder }: InputCustomComponentProps) {
+    return (
+        <div className={`grid w-full max-w-sm items-center gap-1.5`}>
+            <Label htmlFor={htmlfor}>{label}</Label>
+            <Input type={type} id={htmlfor} placeholder={placeholder} />
+        </div>
+    )
+}
